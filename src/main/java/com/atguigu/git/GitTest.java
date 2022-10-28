f402a9d91a557c3ac8803c827c199fa9f7a413e3
Joe <1275903512@qq.com>
2022-10-28 23:11:23 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author linzihao
+ * @create 2022-10-28-23:06
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
